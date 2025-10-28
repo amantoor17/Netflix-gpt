2c848a74a4cce5c29f2a451ba0e23f4b197be770
amantoor17 <amantoordpps@gmail.com>
2025-10-29 02:22:38 +0530

    Added Custom Hooks

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -5,14 +5,14 @@ import { useSelector } from 'react-redux'
 const SecondaryContainer = () => {
 
   const movies = useSelector(store => store.movies);
-  console.log("SECONDARY CONTAINER: ",movies);
+  console.log("SECONDARY CONTAINER POPULAR: ",movies.popularMovies);
 
   return (
     <div className=' bg-black'>
     <div className='-mt-60 relative z-10 pl-4'>
+      <MovieList title={"Trending"} movies={movies.trendingMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies}/>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Favorites"} movies={movies.nowPlayingMovies}/>
     </div>
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,11 +4,19 @@ const moviesSlice = createSlice({
     name: "movies",
     initialState: {
         nowPlayingMovies: null,
+        popularMovies: null,
+        trendingMovies: null,
         trailerVideo: null,
     },
     reducers: {
         addNowPlayingMovies: (state, action) => {
             state.nowPlayingMovies = action.payload;
+        },
+        addPopularMovies: (state, action) => {
+            state.popularMovies = action.payload;
+        },
+        addTrendingMovies: (state, action) => {
+            state.trendingMovies = action.payload;
         },
         addTrailerVideo: (state, action) => {
             state.trailerVideo = action.payload;
@@ -16,6 +24,6 @@ const moviesSlice = createSlice({
      }
 });
 
-export const { addNowPlayingMovies, addTrailerVideo  } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTrendingMovies } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
